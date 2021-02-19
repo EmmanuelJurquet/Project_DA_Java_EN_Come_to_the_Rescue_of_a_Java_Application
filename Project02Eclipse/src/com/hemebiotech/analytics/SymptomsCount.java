02d1c58023cbd93e11db82fa1d0d3601fac4dea9
E.Jurquet <emmanueljurquet@gmail.com>
2021-02-19 11:25:53 +0100

    editing the SymptomsCount and WriteDataOnToFile classes.
    editing the ISymptomWriter interface.

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomsCount.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomsCount.java
@@ -1,5 +1,58 @@
 package com.hemebiotech.analytics;
 
-public class SymptomsCount {
+import java.util.Map;
+import java.util.TreeMap;
 
+import java.util.List;
+
+public class SymptomsCount {
+	/**
+	 * @author E.Jurquet
+	 * 
+	 * @return a list of an HashMap keys are Symptoms and values occurrences for each symptom.
+	 * The keys are listed by alphabetical order.
+	 * 
+	 * This Method uses and instantiate the method "ReadSymptomDataFromFile" and the interface ISymptomReader.  
+	 * @see ReadSymptomDataFromFile
+	 * @see ISymptomReader
+	 * 
+	 * 
+	 */
+	public Map <String, Integer> sCount () {
+
+
+		ISymptomReader lecteur = new ReadSymptomDataFromFile("symptoms.txt");
+
+		List<String> lSymptoms = lecteur.GetSymptoms();
+
+		Map<String,Integer> mSympt = new TreeMap<>();
+
+		try {
+			for (int n = 0; n<lSymptoms.size(); n++) {
+
+				String symptome = lSymptoms.get(n).trim().toLowerCase();
+
+				if (mSympt.containsKey(symptome)) {
+
+					int nbOccurence = mSympt.get(symptome);
+					nbOccurence ++;
+
+
+					mSympt.put(symptome, nbOccurence);
+
+
+				}
+				else {
+					mSympt.put(symptome,1);
+				}
+			}
+			System.out.println(mSympt.toString());
+		}
+		catch (Exception SymptomsCount) {
+			System.out.println("Ceci est une erreur de la methode SymptomCount");
+		}
+
+		return mSympt;
+
+	}
 }
